1f8ef3b321af353d0fa31db6a663b16f4c8be54a
Julie Lykkegaard Graugaard-Jensen <au520066@uni.au.dk>
2018-02-27 15:41:05 +0100

    Tilføjede mapper med Roulette

---
+++ RouletteGame.Test.Unit/Roulette/RouletteUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace RouletteGame.Test.Unit.Roulette
+{
+    [TestFixture]
+    class RouletteUnitTest
+    {
+        [Test]
+        public void Ctor_RouletteNotSpun_GetResultNotAllowed()
+        {
+            var uut = new RouletteGame.
+        }
+    }
+}
