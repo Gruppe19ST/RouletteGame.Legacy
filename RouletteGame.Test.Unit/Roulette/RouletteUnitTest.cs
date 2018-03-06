a06b63e835d4fbd0205ad57510604cc9b9811e77
Anni Lykke Wilhelmsen <201509504@post.au.dk>
2018-03-06 09:25:52 +0100

    Test

--- RouletteGame.Test.Unit/Roulette/RouletteUnitTest.cs
+++ RouletteGame.Test.Unit/Roulette/RouletteUnitTest.cs
@@ -10,10 +10,6 @@ namespace RouletteGame.Test.Unit.Roulette
     [TestFixture]
     class RouletteUnitTest
     {
-        //[Test]
-        //public void Ctor_RouletteNotSpun_GetResultNotAllowed()
-        //{
-        //    var uut = new RouletteGame.
-        //}
+        
     }
 }
